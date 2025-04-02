b2f158f3474dc4f94da30577ae61ca40e81e00b6
IceRedline <scpd2604@gmail.com>
2025-04-02 22:06:43 +0300

    Добавлены кнопки undo и "+" для удаления и добавления эмодзи в коллекцию анимированно

--- EmojiMixer/CollectionViewController.swift
+++ EmojiMixer/CollectionViewController.swift
@@ -9,15 +9,31 @@ import UIKit
 
 class CollectionViewController: UIViewController {
 
-    let emojis = [ "🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🥭", "🍎", "🍏", "🍐", "🍒", "🍓", "🫐", "🥝", "🍅", "🫒", "🥥", "🥑", "🍆", "🥔", "🥕", "🌽", "🌶️", "🫑", "🥒", "🥬", "🥦", "🧄", "🧅", "🍄"]
+    private let emojis = [ "🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🥭", "🍎", "🍏", "🍐", "🍒", "🍓", "🫐", "🥝", "🍅", "🫒", "🥥", "🥑", "🍆", "🥔", "🥕", "🌽", "🌶️", "🫑", "🥒", "🥬", "🥦", "🧄", "🧅", "🍄"]
+    private var visibleEmojis = [String]()
+    private var currentEmojiIndex: Int = 0
     
-    let collection = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    private let collection = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         collection.delegate = self
         collection.dataSource = self
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            title: "undo",
+            style: .plain,
+            target: self,
+            action: #selector(undoTapped)
+        )
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "plus"),
+            style: .plain,
+            target: self,
+            action: #selector(addTapped)
+        )
         
         view.addSubview(collection)
         loadConstraints()
@@ -34,7 +50,46 @@ class CollectionViewController: UIViewController {
             collection.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
         ])
     }
+    
+    private func add(emoji: String) {
+        
+        let count = visibleEmojis.count
+        visibleEmojis.append(emoji)
+        
+        collection.performBatchUpdates {
+            let indexes = (count..<visibleEmojis.count).map { IndexPath(row: $0, section: 0) }
+            collection.insertItems(at: indexes)
+        }
+    }
+    
+    private func remove() {
+        let count = visibleEmojis.count
+        visibleEmojis.removeLast()
+        
+        collection.performBatchUpdates {
+            let indexes = (visibleEmojis.count..<count).map { IndexPath(row: $0, section: 0) }
+            collection.deleteItems(at: indexes)
+        }
+    }
+    
+    @objc private func addTapped() {
+        let selectedEmoji = emojis[currentEmojiIndex]
+        if currentEmojiIndex != emojis.count - 1 {
+            currentEmojiIndex += 1
+        }
+        if visibleEmojis.last != emojis.last {
+            add(emoji: selectedEmoji)
+        }
+    }
+    
+    @objc private func undoTapped() {
+        if !visibleEmojis.isEmpty {
+            remove()
+        }
+    }
 }
+
+// MARK: - UICollectionViewDelegate
 
 extension CollectionViewController: UICollectionViewDelegate {
     
@@ -54,14 +109,19 @@ extension CollectionViewController: UICollectionViewDelegateFlowLayout {
     }
 }
 
+// MARK: - UICollectionViewDataSource
+
 extension CollectionViewController: UICollectionViewDataSource {
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int { emojis.count }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int { visibleEmojis.count }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) //as UICollectionViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.prepareForReuse()
         let label = UILabel()
-        label.text = emojis[indexPath.item]
+        if !visibleEmojis.isEmpty {
+            label.text = visibleEmojis[indexPath.item]
+        }
         cell.addSubview(label)
         label.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
